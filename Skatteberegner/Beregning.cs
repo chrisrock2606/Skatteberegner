5737746dc7fa835e25d0fd17c932bf65d5277fb3
Christian Simonsen <mail@christian-web.dk>
2021-10-03 09:53:32 +0200

    Make tests pass

--- Skatteberegner/Beregning.cs
+++ Skatteberegner/Beregning.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Skatteberegner
+﻿namespace Skatteberegner
 {
     public class Beregning
     {
         // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
         public double SkatVedJulegave(double julegave, double andenGave)
         {
-            return 0;
+            double gaveÅrTotal = julegave + andenGave;
+            double beskatningAfGave = 0;
+            if (gaveÅrTotal > 1200)
+            {
+                beskatningAfGave += andenGave;
+                if (julegave > 900)
+                {
+                    beskatningAfGave += julegave;
+                }
+            }
+
+            return beskatningAfGave;
         }
     }
 }
